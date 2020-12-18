7369a49a781dc1f1c99902f77bbb594c9774c2bc
DaveAlsina <sandavid040@gmail.com>
2020-12-18 13:09:32 -0500

    Modulo de definiciones y documentación general de la librería para la sección que trabaja la toma de mediciones con sensores DHT y DS18

--- AHS_Helper/temp_and_hum.h
+++ AHS_Helper/temp_and_hum.h
@@ -2,9 +2,9 @@
 #define _temp_and_hum_h_
 
 #include <inttypes.h>
-#include "../DHT_sensor_library/DHT.h"
+#include "DHT.h"
 #include <OneWire.h>
-#include "../DallasTemperature/DallasTemperature.h"
+#include "DallasTemperature.h"
 
 
 //INPUT: array of doubles (data), size of array (sz)
@@ -16,18 +16,18 @@ void insertionSort(double* data, uint8_t sz);
 //INPUT: DHT object 
 //OUTPUT: ambiental temperature (double)
 
-double getTemp(DHT obj);	
+double getTemp(DHT obj, uint8_t nMeasurements);	
 
 
-//INPUT: DHT object 
+//INPUT: DHT obj, uint8_t nMeasurementsect 
 //OUTPUT: relative humidity (double)
 
-double getHum(DHT obj); 
+double getHum(DHT obj, uint8_t nMeasurements); 
 
-//INPUT: DallasTemperature object
+//INPUT: DallasTemperature obj, uint8_t nMeasurementsect
 //OUTPUT: measured temperature (double) 
 
-double getTemp(DallasTemperature obj, DeviceAddress addr);
+double getTemp(DallasTemperature obj, const uint8_t* addr, uint8_t nMeasurements);
 
 
 #endif
